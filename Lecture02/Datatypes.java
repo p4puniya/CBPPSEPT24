359a264a8f754c2574f97087837506cca1041f99
G_puniya <60355338+p4puniya@users.noreply.github.com>
2024-09-14 19:18:24 +0530

    Lecture 2

--- Lecture02/Datatypes.java
+++ Lecture02/Datatypes.java
@@ -2,6 +2,22 @@ package Lecture02;
 
 public class Datatypes {
     public static void main(String[] args) {
-        
+        boolean b= true;
+        // int a=-10;
+        int miniInt= Integer.MIN_VALUE;
+        int maxInt= Integer.MAX_VALUE;
+        short sh= -12;
+        short miniSh= Short.MIN_VALUE;
+        short maxSh= Short.MAX_VALUE;
+        float f= 11.0983270091823749187324f;
+        // double d= 11.012378992389472134897324d;
+        char a= 'a';
+        char c='c'; //A = 65, a= 97
+        byte b1= Byte.MIN_VALUE;
+        byte b2= Byte.MAX_VALUE;
+        System.out.println(b1+" "+b2);
+        // System.out.println(maxInt +" "+ maxSh);
+        // System.out.println(miniInt+" "+ miniSh);
+        //11.012378992389472
     }
 }
---
+++ Lecture02/Operators.java
@@ -0,0 +1,11 @@
+package Lecture02;
+
+public class Operators {
+    public static void main(String[] args) {
+        int a=                                
+        
+        10;
+        System.out.println(a++ + " "+ a++ + a);
+        System.out.println(a);
+    }
+}
