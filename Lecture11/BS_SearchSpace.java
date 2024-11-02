c8950458b4be55f5f5d88306d9f36cbee3adec41
G_puniya <60355338+p4puniya@users.noreply.github.com>
2024-11-02 15:45:11 +0530

    Lec 11

--- Lecture11/BS_SearchSpace.java
+++ Lecture11/BS_SearchSpace.java
@@ -19,13 +19,21 @@ public class BS_SearchSpace {
         return ans;
     }
     public static void main(String[] args) {
-        System.out.println(reachCenter(6, 421));
+        // System.out.println(reachCenter(6, 421));
+        int high= Integer.MAX_VALUE, low= 1;
+        System.out.println(Integer.MIN_VALUE);
+        // System.out.println((high+low)/2);
+        // 2147483647
+        // -2147483648  -2147483648
+        // 1073741823
+        // -1073741824
     }
 }
 
 
-//1. Aggresive Cows: 
+//1. Aggresive Cows: Class
 //2. Painter's Patition
 //3. Magnetic Balls: 1552
-//4. Koko Eating Bananas: 857
+//4. Koko Eating Bananas: 875
 //5. Read Books/Book Allocation
+//6. Repair Cars: 2594
