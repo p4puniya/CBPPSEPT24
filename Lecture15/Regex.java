05514c6c9e08044118a24cf0a25739a9d8f9cd78
G_puniya <60355338+p4puniya@users.noreply.github.com>
2024-11-23 12:44:08 +0530

    Lecture 15

---
+++ Lecture15/Regex.java
@@ -0,0 +1,20 @@
+package Lecture15;
+public class Regex {
+    public static void main(String[] args) {
+        /* 
+        \b: Backspace
+        \t: Tab
+        \s: Adding a space
+        \n: New Line
+        \r: Start of the line 
+        \f: Shows a female character but was used for going to the next page
+        \': '
+        \": "
+        \\: \
+        //
+        */
+        String s= "She\ssaid\shi";
+        System.out.println(s);
+
+    }
+}
---
+++ Lecture16/Recursion.java
@@ -0,0 +1,5 @@
+package Lecture16;
+
+public class Recursion {
+    
+}
