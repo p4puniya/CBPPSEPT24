7cfb21c9f4bd79841406ab4f75c747ec4b4961e1
G_puniya <60355338+p4puniya@users.noreply.github.com>
2024-12-28 14:55:45 +0530

    LinkedList

--- Lecture22/LinkedList.java
+++ Lecture22/LinkedList.java
@@ -36,6 +36,41 @@ public class LinkedList {
         }
     }
 
+    public Node getNode(int k){
+        Node nn= head;
+        for(int i=0;i<k;i++)
+            nn= nn.next;
+        return nn;
+    }
+
+    public void addAtIndex(int item, int k){
+        if(k==0) addFirst(item);
+        else if(k==size) addLast(item);
+        else{
+            Node nn= new Node();
+            nn.val= item;
+            Node prev= getNode(k-1);
+            nn.next= prev.next;
+            prev.next= nn;
+            size++;
+        }
+    }
+
+    public int getFirst(){
+        return head.val;
+    }
+
+    public int getLast(){
+        return tail.val;
+    }
+
+    public int getIndex(int k){
+        if(k<0 || k>=size)
+            return -1;
+        if(k==0)return getFirst();
+        else if(k==size-1)return getLast();
+        else return getNode(k).val;
+    }
     public void display(){
         Node temp =head;
         while(temp!=null){
